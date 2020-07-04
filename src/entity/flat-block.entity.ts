41e5b9c73f560d7b5c85d3b8b769636b8231b36d
Roman Kuleshov <rkuleshov@morebis.pl>
2020-07-04 11:53:33 +0300

    Added kitchen room matrix

--- src/entity/flat-block.entity.ts
+++ src/entity/flat-block.entity.ts
@@ -1,5 +1,26 @@
 import { SpriteEntity } from '../core/sprite.entity';
 
+export enum HouseParticles{
+  Wall,
+  FreeSpace,
+  Window,
+  Door,
+}
+
+const kitchenRoom: Array<any> = [
+  [HouseParticles.Wall, HouseParticles.Wall, HouseParticles.Wall, HouseParticles.Wall, HouseParticles.Window, HouseParticles.Window, HouseParticles.Window, HouseParticles.Wall, HouseParticles.Wall, HouseParticles.Wall, HouseParticles.Wall, ],
+  [HouseParticles.Wall, HouseParticles.FreeSpace, HouseParticles.FreeSpace, HouseParticles.FreeSpace, HouseParticles.FreeSpace, HouseParticles.FreeSpace, HouseParticles.FreeSpace, HouseParticles.FreeSpace, HouseParticles.FreeSpace, HouseParticles.FreeSpace,,HouseParticles.Wall],
+  [HouseParticles.Wall, HouseParticles.FreeSpace, HouseParticles.FreeSpace, HouseParticles.FreeSpace, HouseParticles.FreeSpace, HouseParticles.FreeSpace, HouseParticles.FreeSpace, HouseParticles.FreeSpace, HouseParticles.FreeSpace, HouseParticles.FreeSpace,,HouseParticles.Wall],
+  [HouseParticles.Wall, HouseParticles.FreeSpace, HouseParticles.FreeSpace, HouseParticles.FreeSpace, HouseParticles.FreeSpace, HouseParticles.FreeSpace, HouseParticles.FreeSpace, HouseParticles.FreeSpace, HouseParticles.FreeSpace, HouseParticles.FreeSpace,,HouseParticles.Wall],
+  [HouseParticles.Window, HouseParticles.FreeSpace,  HouseParticles.FreeSpace,  HouseParticles.FreeSpace,  HouseParticles.FreeSpace,  HouseParticles.FreeSpace,  HouseParticles.FreeSpace,  HouseParticles.FreeSpace,  HouseParticles.FreeSpace,  HouseParticles.FreeSpace, HouseParticles.Door],
+  [HouseParticles.Window, HouseParticles.FreeSpace,  HouseParticles.FreeSpace,  HouseParticles.FreeSpace,  HouseParticles.FreeSpace,  HouseParticles.FreeSpace,  HouseParticles.FreeSpace,  HouseParticles.FreeSpace,  HouseParticles.FreeSpace,  HouseParticles.FreeSpace, HouseParticles.Door],
+  [HouseParticles.Window, HouseParticles.FreeSpace,  HouseParticles.FreeSpace,  HouseParticles.FreeSpace,  HouseParticles.FreeSpace,  HouseParticles.FreeSpace,  HouseParticles.FreeSpace,  HouseParticles.FreeSpace,  HouseParticles.FreeSpace,  HouseParticles.FreeSpace, HouseParticles.Door],
+  [HouseParticles.Wall, HouseParticles.FreeSpace, HouseParticles.FreeSpace, HouseParticles.FreeSpace, HouseParticles.FreeSpace, HouseParticles.FreeSpace, HouseParticles.FreeSpace, HouseParticles.FreeSpace, HouseParticles.FreeSpace, HouseParticles.FreeSpace,,HouseParticles.Wall],
+  [HouseParticles.Wall, HouseParticles.FreeSpace, HouseParticles.FreeSpace, HouseParticles.FreeSpace, HouseParticles.FreeSpace, HouseParticles.FreeSpace, HouseParticles.FreeSpace, HouseParticles.FreeSpace, HouseParticles.FreeSpace, HouseParticles.FreeSpace,,HouseParticles.Wall],
+  [HouseParticles.Wall, HouseParticles.FreeSpace, HouseParticles.FreeSpace, HouseParticles.FreeSpace, HouseParticles.FreeSpace, HouseParticles.FreeSpace, HouseParticles.FreeSpace, HouseParticles.FreeSpace, HouseParticles.FreeSpace, HouseParticles.FreeSpace,,HouseParticles.Wall],
+  [HouseParticles.Wall, HouseParticles.Wall, HouseParticles.Wall, HouseParticles.Wall, HouseParticles.Door,  HouseParticles.Door,  HouseParticles.Door,  HouseParticles.Door, HouseParticles.Wall, HouseParticles.Wall, HouseParticles.Wall, HouseParticles.Wall]
+];
+
 export class FlatBlockEntity extends SpriteEntity {
 
   constructor(scene: Phaser.Scene, x: number, y: number, key: string) {
