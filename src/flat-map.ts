ae501347f838c30699953e37cbfd7fd60e42a35d
Roman Kuleshov <rkuleshov@morebis.pl>
2020-07-04 18:42:37 +0300

    Added room group

--- src/flat-map.ts
+++ src/flat-map.ts
@@ -1,5 +1,7 @@
 import gameConfig from './core/game.config';
 import { EHouseParticles, FlatBlockEntity } from './entity/flat-block.entity';
+import { Point } from "./core/point";
+import { RoomGroup } from "./groups/room.group";
 
 const sprayMap = [
   'gray', 'white', 'red', 'green'
@@ -33,7 +35,22 @@ const houseStringMap: any =
   '####@@@######==###########@@@@@@#';
 
 
-export class FlatMap {
+export enum EHouseGroup{
+  Kitchen,
+  Bedroom,
+  Wardrobe,
+  Garage,
+  Bathroom
+}
+
+export interface ITileEntity{
+  cordX: number;
+  cordY: number;
+  houseParticleType: EHouseParticles;
+  particleGroup: EHouseGroup;
+}
+
+export class FlatMap{
 
   generatedBlocks: FlatBlockEntity[][];
 
@@ -48,6 +65,8 @@ export class FlatMap {
   generateFlatSpriteBlocks(scene: Phaser.Scene) {
     const blockWidth = gameConfig.width / this.regenerateMapSymbolToEnum()[0].length;
     const blockHeight = gameConfig.height / this.regenerateMapSymbolToEnum().length;
+
+    this.sectorChecker();
 
     this.generatedBlocks = this.regenerateMapSymbolToEnum().map((row, y) => {
       return row.map((blockType, x) => {
@@ -77,5 +96,118 @@ export class FlatMap {
 
 
     return complitedMap;
+  }
+
+
+  // aweqwe() {
+  //   {
+  //     cordX: ;
+  //     cordY: ;
+  //     criticalUpXY: ;
+  //     criticalUpYX: ;
+  //     criticalDownXY: ;
+  //     criticalDownYX: ;
+  //     section: ;
+  //     type: ;
+  //   }
+  // }
+
+
+  sectorChecker():RoomGroup[] {
+    const map = this.regenerateMapSymbolToEnum();
+
+    let fullGroupMap: RoomGroup[] = [];
+
+    for (let cordY = 1; cordY < map.length - 2; cordY++) {
+      for (let cordX = 1; cordX < map[cordY].length - 2; cordX++) {
+        let standingTile = map[cordY][cordX];
+
+        if(standingTile == EHouseParticles.FreeSpace) {
+          //get neibor tiles
+          let tilesOnSides: Point[] = [];
+          //todo
+          if(map[cordY - 1][cordX] == EHouseParticles.FreeSpace)
+            tilesOnSides.push(new Point(cordX, cordY - 1));//top
+
+          if(map[cordY + 1][cordX] == EHouseParticles.FreeSpace)
+            tilesOnSides.push(new Point(cordX, cordY + 1));//bottom
+
+          if(map[cordY][cordX - 1] == EHouseParticles.FreeSpace)
+            tilesOnSides.push(new Point(cordX - 1, cordY));//left
+
+          if(map[cordY][cordX + 1] == EHouseParticles.FreeSpace)
+            tilesOnSides.push(new Point(cordX + 1, cordY));//right
+
+          let isGroupWasFound = false;
+
+          for (let j = 0; j < fullGroupMap.length ; j++) {
+            let tl: Point = tilesOnSides.find(element => fullGroupMap[j].groupMap.find(x => x.x == element.x && x.y == element.y));
+
+            if(tl !== undefined) {
+              fullGroupMap[j].groupMap.push(new Point(cordX, cordY));
+              isGroupWasFound = true;
+              //group has found
+              break;
+            }
+          }
+
+          if(isGroupWasFound == false)
+          {
+            let newGroup = new RoomGroup();
+            newGroup.groupMap.push(new Point(cordX, cordY));
+
+            fullGroupMap.push(newGroup);
+
+          }
+        }
+
+//--------------------------------------------------------------------------------------------------
+        if(standingTile == EHouseParticles.Door) {
+          //get neibor tiles
+          let doorsOnSides: Point[] = [];
+          //todo
+          if(map[cordY - 1][cordX] == EHouseParticles.Door)
+            doorsOnSides.push(new Point(cordX, cordY - 1));//top
+
+          if(map[cordY + 1][cordX] == EHouseParticles.Door)
+            doorsOnSides.push(new Point(cordX, cordY + 1));//bottom
+
+          if(map[cordY][cordX - 1] == EHouseParticles.Door)
+            doorsOnSides.push(new Point(cordX - 1, cordY));//left
+
+          if(map[cordY][cordX + 1] == EHouseParticles.Door)
+            doorsOnSides.push(new Point(cordX + 1, cordY));//right
+
+          let isGroupWasFound = false;
+
+          for (let j = 0; j < fullGroupMap.length ; j++) {
+            let tl: Point = doorsOnSides.find(element => fullGroupMap[j].groupMap.find(x => x.x == element.x && x.y == element.y));
+
+            if(tl !== undefined) {
+              fullGroupMap[j].groupMap.push(new Point(cordX, cordY));
+              isGroupWasFound = true;
+              //group has found
+              break;
+            }
+          }
+
+          if(isGroupWasFound == false)
+          {
+            let newGroup = new RoomGroup();
+            newGroup.groupMap.push(new Point(cordX, cordY));
+
+            fullGroupMap.push(newGroup);
+
+          }
+        }
+//-----------------------------------------------------------------------------
+
+      }
+
+    }
+
+
+    console.log('fullGroupMap', fullGroupMap);
+    return  fullGroupMap;
   }
 }
--- src/groups/room.group.ts
+++ src/groups/room.group.ts
@@ -1,9 +1,26 @@
 import { IElectricityObject } from '../core/interfaces';
+import { Point } from "../core/point";
 
-export class RoomGroup extends Phaser.GameObjects.Group implements IElectricityObject {
+export class RoomGroup  {
+
+
+  groupMap: Point[];
+  groupDoors: Doors[];
+  groupNumber: number;
 
   get consumePerTick() {
+    var obj: Doors = {
+      key1: new Point(0,0),
+      key2: 123
+    };
+
+    this.groupDoors.push(obj);
     return 0; // TODO
   }
 
+
+}
+
+interface Doors {
+  [key: string]: Point | number ;
 }
