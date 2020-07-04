b7aa1e25a0a7a71220ec36529456fc6b7bb71715
Roman Kuleshov <rkuleshov@morebis.pl>
2020-07-04 14:10:24 +0300

    Added house

--- src/flat-map.ts
+++ src/flat-map.ts
@@ -8,6 +8,33 @@ export enum HouseParticles {
   Door,
 }
 
+const houseStringMap: any =
+  '####@@@######@@@####@@@#####@@###\n' +
+  '#.........#..............#......#\n' +
+  '#.........#..............#......#\n' +
+  '#.........#..............=......#\n' +
+  '@.........=..............=......#\n' +
+  '@.........=..............#......#\n' +
+  '@.........=..............#......#\n' +
+  '#.........#..............###==###\n' +
+  '#.........#..............#......#\n' +
+  '#.........#..............#......#\n' +
+  '####===######==###########......#\n' +
+  '#.........#......#.......#......#\n' +
+  '#.........#......#.......#......#\n' +
+  '#.........#......#.......#......#\n' +
+  '@.........=......=.......#......#\n' +
+  '@.........=......=.......#......#\n' +
+  '#.........#......#.......#......#\n' +
+  '#.........#......#.......#......#\n' +
+  '#.........#......#.......#......#\n' +
+  '@.........#......#########......#\n' +
+  '@.........#......#.......#......#\n' +
+  '#.........#......=.......#......#\n' +
+  '#.........#......=.......#......#\n' +
+  '#.........#......#.......#......#\n' +
+  '####@@@######==###########@@@@@@#';
+
 const kitchenRoom: HouseParticles[][] = [
   [HouseParticles.Wall, HouseParticles.Wall, HouseParticles.Wall, HouseParticles.Wall, HouseParticles.Window, HouseParticles.Window, HouseParticles.Window, HouseParticles.Wall, HouseParticles.Wall, HouseParticles.Wall, HouseParticles.Wall,],
   [HouseParticles.Wall, HouseParticles.FreeSpace, HouseParticles.FreeSpace, HouseParticles.FreeSpace, HouseParticles.FreeSpace, HouseParticles.FreeSpace, HouseParticles.FreeSpace, HouseParticles.FreeSpace, HouseParticles.FreeSpace, HouseParticles.FreeSpace, , HouseParticles.Wall],
@@ -22,20 +49,40 @@ const kitchenRoom: HouseParticles[][] = [
   [HouseParticles.Wall, HouseParticles.Wall, HouseParticles.Wall, HouseParticles.Wall, HouseParticles.Door, HouseParticles.Door, HouseParticles.Door, HouseParticles.Door, HouseParticles.Wall, HouseParticles.Wall, HouseParticles.Wall, HouseParticles.Wall]
 ];
 
+
 export class FlatMap {
 
   getMap() {
-    return kitchenRoom;
+    return this.regenerateMapSymbolToEnum();
   }
 
   generateFlatSpriteBlocks(scene: Phaser.Scene) {
-    const blockWidth = gameConfig.width / kitchenRoom[0].length;
-    const blockHeight = gameConfig.height / kitchenRoom.length;
+    const blockWidth = gameConfig.width / this.regenerateMapSymbolToEnum()[0].length;
+    const blockHeight = gameConfig.height / this.regenerateMapSymbolToEnum().length;
 
-    return kitchenRoom.map((row, x) => {
+    return this.regenerateMapSymbolToEnum().map((row, x) => {
       return row.map((blockPice, y) => {
         return new FlatBlockEntity(scene, x * blockWidth, y * blockHeight, 'white');
       })
     });
+  }
+
+  regenerateMapSymbolToEnum(): HouseParticles[][] {
+    let complitedMap: HouseParticles[][] = [];
+
+    let enumTypeMap = houseStringMap
+      .trim()
+      .replace(/#/g, HouseParticles.Wall)
+      .replace(/=/g, HouseParticles.Door)
+      .replace(/@/g, HouseParticles.Window)
+      .replace(/\./g, HouseParticles.FreeSpace)
+      .match(/.{1,33}/g);
+
+    enumTypeMap.forEach((houseLine: string) => {
+      complitedMap.push([...<any>houseLine]);
+    });
+
+
+    return complitedMap;
   }
 }
