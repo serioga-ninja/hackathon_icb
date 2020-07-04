3285c94130add7ea5295a7ae195e5abe0684647d
Serhii Datsii <sdatsiy@morebis.pl>
2020-07-04 10:53:59 +0300

    game scene

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
-alert('Hello');
+import 'phaser';
+import gameConfig from './core/game.config';
+import GameConfig = Phaser.Types.Core.GameConfig;
+
+const config: GameConfig = {
+    title: 'SmartHome',
+    width: gameConfig.width,
+    height: gameConfig.height,
+    parent: 'game',
+    scene: [],
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: true
+        }
+    },
+    backgroundColor: '#a3a3a3'
+};
+
+export class SmartHouseGame extends Phaser.Game {
+    constructor(config: GameConfig) {
+        super(config);
+    }
+}
+
+window.onload = () => {
+    const game = new SmartHouseGame(config);
+    document.getElementById('game').focus();
+};
